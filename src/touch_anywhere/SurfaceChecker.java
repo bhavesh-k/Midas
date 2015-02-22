14ffb55f25b30678b67dbbe993bda281ada81c94
Maro-Kun <maro.kun.github@gmail.com>
2015-02-22 10:39:32 -0500

    cours d'algo c là pis toute?
    angular c'Est amazing your friends use io.js should wait guys.Math question est-ce qu'elle travaille un false non? idk lol non j'installe quoi vendredi?

--- src/touch_anywhere/SurfaceChecker.java
+++ src/touch_anywhere/SurfaceChecker.java
@@ -21,26 +21,25 @@ public class SurfaceChecker {
 		
 		for (DrawnButton element : buttonsList) {
 			float min_x = element.topLeft.getX();			
-			float max_y = element.topLeft.getY();
 			float max_x = element.bottomRight.getX();
-			float min_y = element.bottomRight.getY();
+				float min_y = element.bottomRight.getY();
 			
 			float curr_x = position.getX(); float curr_y = position.getY();
 			
+			if ((curr_x < max_x) && (curr_x > min_x) && (curr_y < max_y) && (curr_y > min_y)) { //check x-y position
 			if ((curr_x < max_x) && (curr_x > min_x) && (curr_y < max_y) && (curr_y > min_y)) { //check x-y position
 				
 				if ((position.getZ() - element.averageZ) < 5.0) {//check z position within bounds	
 					element.doMethod(element.buttonName);
 					return (element.buttonName);
 				}
-				
+     //				
 			}
 		}
 		
 		return ("None");
 	}
-	
-	//everytime calibrator has new button in it
+       	//everytime calibrator has new button in it
 	public void update(SurfaceCalibrator calibrator) {
 		this.buttonsList = calibrator.buttonsList;
 	}
